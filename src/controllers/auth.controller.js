ec1d9807cc92033dc76a5e32666d071ae51e357f
ShyamPrakashPPK <shyamprakash0019@gmail.com>
2025-08-27 13:37:33 +0530

    Enhance user registration by adding avatar assignment to the user object

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,10 +7,10 @@ export const register = async (req, res, next) => {
     const exists = await User.findOne({ email });
     if (exists) return res.status(409).json({ message: 'Email already in use' });
     const avatar = Math.floor(Math.random() * 3) + 1; 
-    const user = await User.create({ name, email, password });
+    const user = await User.create({ name, email, password, avatar });
     const token = signToken({ sub: user._id.toString(), role: user.role });
     res.status(201).json({
-      user: { id: user._id, name: user.name, email: user.email, role: user.role ,  avatar: user.avatar },
+      user: { id: user._id, name: user.name, email: user.email, role: user.role ,  avatar: avatar },
       token
     });
   } catch (e) {
